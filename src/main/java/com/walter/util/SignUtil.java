ac8cde4831abc04685e8dde96e1eb60930d4eef0
tanyunxing <tanyunxing@yy.com>
2020-08-19 19:39:24 +0800

    添加敏感信息加解密方法

--- src/main/java/com/walter/util/SignUtil.java
+++ src/main/java/com/walter/util/SignUtil.java
@@ -3,13 +3,14 @@ package com.walter.util;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.security.KeyFactory;
-import java.security.NoSuchAlgorithmException;
-import java.security.PrivateKey;
-import java.security.Signature;
+import java.security.*;
 import java.security.cert.*;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
@@ -69,11 +70,12 @@ public class SignUtil {
 
     /**
      * 签名
-     * @param pkFile	私钥文件
-     * @param requestData	请求参数
+     *
+     * @param pkFile      私钥文件
+     * @param requestData 请求参数
      * @return
      */
-    public static String sign(File pkFile, String requestData){
+    public static String sign(File pkFile, String requestData) {
         String signature = null;
         byte[] signed;
         try {
@@ -94,11 +96,12 @@ public class SignUtil {
 
     /**
      * 签名
-     * @param pkContent	私钥文件内容
-     * @param requestData	请求参数
+     *
+     * @param pkContent   私钥文件内容
+     * @param requestData 请求参数
      * @return
      */
-    public static String sign(String pkContent, String requestData){
+    public static String sign(String pkContent, String requestData) {
         String signature = null;
         byte[] signed;
         try {
@@ -140,6 +143,57 @@ public class SignUtil {
             throw new RuntimeException("无效的证书文件", e);
         } finally {
             bis.close();
+        }
+    }
+
+    /**
+     * 敏感信息加解密，用于上传数据到微信
+     *
+     * @param filename 证书文件路径  (required)
+     * @return X509证书
+     */
+    public static String rsaEncryptOAEP(String message, String filename)
+            throws Exception {
+        try {
+            X509Certificate certificate = getCertificate(filename);
+            Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA-1AndMGF1Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, certificate.getPublicKey());
+
+            byte[] data = message.getBytes("utf-8");
+            byte[] cipherdata = cipher.doFinal(data);
+            return Base64.getEncoder().encodeToString(cipherdata);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+            throw new RuntimeException("当前Java环境不支持RSA v1.5/OAEP", e);
+        } catch (InvalidKeyException e) {
+            throw new IllegalArgumentException("无效的证书", e);
+        } catch (IllegalBlockSizeException | BadPaddingException e) {
+            throw new IllegalBlockSizeException("加密原串的长度不能超过214字节");
+        }
+    }
+
+    /**
+     *
+     * @param ciphertext
+     * @param pkFile
+     * @return
+     * @throws BadPaddingException
+     * @throws IOException
+     */
+    public static String rsaDecryptOAEP(String ciphertext, File pkFile)
+            throws BadPaddingException, IOException {
+        try {
+            PrivateKey privateKey = getPrivateKey(pkFile);
+            Cipher cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA-1AndMGF1Padding");
+            cipher.init(Cipher.DECRYPT_MODE, privateKey);
+
+            byte[] data = Base64.getDecoder().decode(ciphertext);
+            return new String(cipher.doFinal(data), "utf-8");
+        } catch (NoSuchPaddingException | NoSuchAlgorithmException e) {
+            throw new RuntimeException("当前Java环境不支持RSA v1.5/OAEP", e);
+        } catch (InvalidKeyException e) {
+            throw new IllegalArgumentException("无效的私钥", e);
+        } catch (BadPaddingException | IllegalBlockSizeException e) {
+            throw new BadPaddingException("解密失败");
         }
     }
 }
--- src/test/java/com/walter/SignTest.java
+++ src/test/java/com/walter/SignTest.java
@@ -3,7 +3,9 @@ package com.walter;
 import com.walter.util.SignUtil;
 import org.junit.Test;
 
+import javax.crypto.BadPaddingException;
 import java.io.File;
+import java.io.IOException;
 
 public class SignTest {
 
@@ -25,5 +27,19 @@ public class SignTest {
     public void serialNumber() throws Exception {
         String pcf = "D:/apiclient_cert.pem";
         System.out.println(SignUtil.getCertificate(pcf).getSerialNumber());
+    }
+
+    @Test
+    public void rsaEncryptOAEP() throws Exception {
+        String filename = "D:/apiclient_cert.pem";
+        String message = "HelloWorld";
+        System.out.println(SignUtil.rsaEncryptOAEP(message, filename));
+    }
+
+    @Test
+    public void rsaDecryptOAEP() throws BadPaddingException, IOException {
+        File pkFile = new File("D:/apiclient_key.pem");
+        String message = "TtWy6vO0HPGJsPo5tZYdEMfTUMJ0FGrYGgQhjRe2gw5S62IrINAEQfnuDtipDaLb34xJmycxrztWrrQvYTkxNziCbWpthGNLQGVNAseIA3GJOHcCd6euH4jFPP7ZF7D25DEXBo6Y5EB4F6FYWb7z29NnIQ2CqmrYxbQZbYlSNP9taqiPOQ7YBfLTWHr3y8k/BLyQwUZCZ6TNt6/CRJUAXrAWOYejLFtTqB0QABmoqzcPUX1QT33uMYTdsPjKwbn1zxb7vJtNmqEmzvG69QM58AoCeykv8oaAmiBJHiVwkGHe0QQePxGBZHjhR7R2ep212EotZBhwYbBNdEtTlTrj2w==";
+        System.out.println(SignUtil.rsaDecryptOAEP(message, pkFile));
     }
 }
